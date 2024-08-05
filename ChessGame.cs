a17b785c55bed9653727680d9072a349080b320e
kneecapFolder <ilan7gr@gmail.com>
2024-08-05 15:37:29 +0300

    added all pieces

---
+++ ChessGame.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Pieces;
+
+namespace MainProgram;
+
+public class Chess : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    ButtonState oldState = ButtonState.Released;
+    Texture2D spriteSheet;
+    Texture2D square;
+    Piece selected = null;
+
+    public Chess()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        _graphics.PreferredBackBufferWidth = 560;
+        _graphics.PreferredBackBufferHeight = 560;
+        _graphics.ApplyChanges();
+
+        // Create a blank square
+        square = new Texture2D(GraphicsDevice, 1, 1);
+        square.SetData(new[] { Color.White });
+
+        Piece.board = new() {
+            new Rook(new(4, 4), Team.Black),
+            // Black
+            new Rook(new(0, 0), Team.Black),
+            new Knight(new(1, 0), Team.Black),
+            new Bishop(new(2, 0), Team.Black),
+            new Queen(new(3, 0), Team.Black),
+            new King(new(4, 0), Team.Black),
+            new Bishop(new(5, 0), Team.Black),
+            new Knight(new(6, 0), Team.Black),
+            new Rook(new(7, 0), Team.Black),
+            new Pawn(new(0, 1), Team.Black),
+            new Pawn(new(1, 1), Team.Black),
+            new Pawn(new(2, 1), Team.Black),
+            new Pawn(new(3, 1), Team.Black),
+            new Pawn(new(4, 1), Team.Black),
+            new Pawn(new(5, 1), Team.Black),
+            new Pawn(new(6, 1), Team.Black),
+            new Pawn(new(7, 1), Team.Black),
+            
+            // White
+            new Pawn(new(0, 6), Team.White),
+            new Pawn(new(1, 6), Team.White),
+            new Pawn(new(2, 6), Team.White),
+            new Pawn(new(3, 6), Team.White),
+            new Pawn(new(4, 6), Team.White),
+            new Pawn(new(5, 6), Team.White),
+            new Pawn(new(6, 6), Team.White),
+            new Pawn(new(7, 6), Team.White),
+            new Rook(new(0, 7), Team.White),
+            new Knight(new(1, 7), Team.White),
+            new Bishop(new(2, 7), Team.White),
+            new Queen(new(3, 7), Team.White),
+            new King(new(4, 7), Team.White),
+            new Bishop(new(5, 7), Team.White),
+            new Knight(new(6, 7), Team.White),
+            new Rook(new(7, 7), Team.White),
+        };
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+        spriteSheet = Content.Load<Texture2D>("sprites/pieces");
+        Piece._spriteBatch = _spriteBatch;
+        Piece.spriteSheet = spriteSheet;
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+        ButtonState newState = Mouse.GetState().LeftButton;
+
+        if (newState == ButtonState.Pressed && oldState == ButtonState.Released) {
+            int x = (int)Map(Mouse.GetState().X - 20, 0, 520, 0, 8);
+            int y = (int)Map(Mouse.GetState().Y - 20, 0, 520, 0, 8);
+            if (selected != null && selected.pos.Equals(new Vector2(x, y)))
+                selected = null;
+            else {
+                selected = null;
+                foreach(Piece p in Piece.board)
+                    if (p.pos.Equals(new Vector2(x, y)))
+                        selected = p;
+            }
+        }
+
+        oldState = newState;
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        _spriteBatch.Begin();
+
+        // Draw board grid
+        _spriteBatch.Draw(square, new Rectangle(
+            20, 20, 520, 520
+        ), Color.MediumSeaGreen);
+        for(int y = 0; y < 8; y++)
+            for(int x = y%2; x < 8; x += 2)
+                _spriteBatch.Draw(
+                    square, new Rectangle(
+                        x * 65 + 20, y * 65 + 20, 65, 65
+                    ), Color.Beige
+                );
+
+        foreach(Piece p in Piece.board)
+            p.Draw();
+
+        if (selected != null)
+            foreach(Vector2 v in selected.GetAvailable())
+                _spriteBatch.Draw(square, new Rectangle((int)v.X*65+20, (int)v.Y*65+20, 20, 20), Color.Red);
+                
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    private float Map(float n, float start1, float end1, float start2, float end2) {
+        return (n-start1)/(end1-start1)*(end2-start2)+start2;
+    }
+}
--- classes/Pieces.cs
+++ classes/Pieces.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,12 +11,13 @@ enum Team {
     White, Black
 }
 
-// Base piece class
 abstract class Piece {
     // Piece size in pixels on the spritesheet
-    protected readonly int size = 75;
+    protected readonly static int size = 65;
+    public static Texture2D spriteSheet;
+    public static SpriteBatch _spriteBatch;
+    public static List<Piece> board;
 
-    // Check that pos is in board
     public Vector2 pos;
     public Team team;
     public List<Vector2> available = new(); // List of available possitions
@@ -23,24 +26,164 @@ abstract class Piece {
     public Piece(Vector2 pos, Team team) {
         this.pos = pos;
         this.team = team;
-        source = new Rectangle(0, size*(int)team, size, size);
+        source = new Rectangle(0, size * (int)team, size, size);
+    }
+
+    protected bool IsAvailable(Vector2 _pos, bool sameTeam = true) {
+        if (this is Pawn) {
+            return false;
+        }
+        else if (sameTeam)
+            return !board.Any(p => p.pos.Equals(_pos) && p.team == team);
+        else return !board.Any(p => p.pos.Equals(_pos) && p.team != team);
+    }
+
+    protected List<Vector2> CleanAvailable() {
+        available.Remove(pos);
+        List<Vector2> toRemove = new();
+        foreach(Vector2 v in available)
+            if (v.X < 0 || v.X >= 8 || v.Y < 0 || v.Y >= 8)
+                toRemove.Add(v);
+        available.RemoveAll(v => toRemove.Contains(v));
+        return available;
+    }
+
+    protected List<Vector2> GetLines(Vector2[] transforms) {
+        available.Clear();
+        bool[] finishedPaths = new bool[transforms.Length];
+        for(int i = 1; i < 8; i++) {
+            for(int j = 0; j < 4; j++) {
+                if (!finishedPaths[j]) {
+                    Vector2 _pos = pos + Vector2.Multiply(transforms[j], i);
+                    bool stopped = false;
+                    foreach(Piece p in board) {
+                        if (p.pos.Equals(_pos)) {
+                            if (p.team != team)
+                                available.Add(_pos);
+                            finishedPaths[j] = true;
+                            stopped = true;
+                            break;
+                        }
+                    }
+                    if (!stopped && IsAvailable(_pos))
+                        available.Add(_pos);
+                }
+            }
+        }
+        return CleanAvailable();
+    }
+
+    public void Draw() {
+        _spriteBatch.Draw(
+            spriteSheet, new Rectangle(
+                (int)pos.X * 65 + 20, (int)pos.Y * 65 + 20, 65, 65
+            ), source, Color.White
+        );
     }
 
     public abstract List<Vector2> GetAvailable();
+}
+
+sealed class King : Piece {
+    public King(Vector2 pos, Team team) : base(pos, team) {
+        source.X = 0;
+    }
+
+    public override List<Vector2> GetAvailable() {
+        available.Clear();
+        for(int y = -1; y <= 1; y++)
+            for(int x = -1; x <= 1; x++)
+                if (IsAvailable(pos + new Vector2(x, y)))
+                    available.Add(pos + new Vector2(x, y));
+        return CleanAvailable();
+    }
+}
+
+sealed class Queen : Piece {
+    public Queen(Vector2 pos, Team team) : base(pos, team) {
+        source.X = size;
+    }
+
+    public override List<Vector2> GetAvailable() {
+        return GetLines(new Vector2[]{
+            new Vector2(1, 0),
+            new Vector2(-1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1),
+            new Vector2(1, 1),
+            new Vector2(-1, 1),
+            new Vector2(1, -1),
+            new Vector2(-1, -1)
+        });
+    }
+}
+
+sealed class Bishop : Piece {
+    public Bishop(Vector2 pos, Team team) : base(pos, team) {
+        source.X = 2*size;
+    }
+
+    public override List<Vector2> GetAvailable() {
+        return GetLines(new Vector2[]{
+            new Vector2(1, 1),
+            new Vector2(-1, 1),
+            new Vector2(1, -1),
+            new Vector2(-1, -1)
+        });
+    }
+}
+
+sealed class Knight : Piece {
+    public Knight(Vector2 pos, Team team) : base(pos, team) {
+        source.X = 3*size;
+    }
+
+    public override List<Vector2> GetAvailable() {
+        available.Clear();
+        Vector2[] positions = {
+            new Vector2(1, 2),
+            new Vector2(2, 1),
+            new Vector2(2, -1),
+            new Vector2(1, -2),
+            new Vector2(-1, -2),
+            new Vector2(-2, -1),
+            new Vector2(-2, 1),
+            new Vector2(-1, 2),
+        };
+        foreach(Vector2 v in positions)
+            if (IsAvailable(pos + v))
+                available.Add(pos + v);
+        return CleanAvailable();
+    }
+}
+
+sealed class Rook : Piece {
+    public Rook(Vector2 pos, Team team) : base(pos, team) {
+        source.X = 4*size;
+    }
+
+    public override List<Vector2> GetAvailable() {
+        return GetLines(new Vector2[]{
+            new Vector2(1, 0),
+            new Vector2(-1, 0),
+            new Vector2(0, 1),
+            new Vector2(0, -1)
+        });
+    }
 }
 
 sealed class Pawn : Piece {
     bool hasMoved = false;
 
     public Pawn(Vector2 pos, Team team) : base(pos, team) {
-        source.X = 0;
+        source.X = 5*size;
     }
 
     public override List<Vector2> GetAvailable() {
         available.Clear();
-        if (hasMoved)
-            available.Add(pos + new Vector2(0, (int)team*-1*-2));
-        available.Add(pos + new Vector2(0, (int)team*-1*-1));
-        return available;
+        if (!hasMoved)
+            available.Add(pos + new Vector2(0, team == Team.White? -2 : 2));
+        available.Add(pos + new Vector2(0, team == Team.White? -1 : 1));
+        return CleanAvailable();
     }
 }
